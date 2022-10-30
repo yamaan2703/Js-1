bbc32555dec7df31385938f04d34f774ad47bd74
yamaan2703 <115937602+yamaan2703@users.noreply.github.com>
2022-10-30 12:04:45 +0500

    commit

---
+++ app.js
@@ -0,0 +1,33 @@
+alert("Thanks For Your Input")
+window.alert("Thanks For Your Input")
+ 
+
+
+var a = 25;
+var b = 15;
+// var c = a + b;
+// var c = a * b;
+// var c = a / b;
+var c = a - b;
+console.log(c)
+
+ var a = 10
+ var b = a + 20
+ console.log (b)
+
+
+ var a = 10
+ a++
+ console.log(a)
+
+ var b = 10
+ ++b
+ console.log(b)
+
+ var c = 10
+ c--
+ console.log(c)
+
+ var c = 10
+ --c
+ console.log(c)
